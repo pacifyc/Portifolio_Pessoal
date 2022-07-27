1c9a912f9e3f6bb4bd4d910afedb8541df30ba98
Silvio_Sena <silviosenna.jl@gmail>
2022-07-27 00:26:14 -0300

    Alterações para Menu
    Foi efetuadas alterações para menu lateral

--- portifolio_pessoal/assets/js/scripts.js
+++ portifolio_pessoal/assets/js/scripts.js
@@ -1,3 +1,19 @@
+
+let botao_menu = document.querySelector('#menu-btn');
+let menu_lateral = document.querySelector('.menu');
+
+
+botao_menu.onclick = () =>{
+    botao_menu.classList.toggle('fa-times');
+    menu_lateral.classList.toggle('active');
+    
+}
+
+window.onscroll = () =>{
+    botao_menu.classList.remove('fa-times');
+    menu_lateral.classList.remove('active');
+}
+
 const body = document.querySelector('body'),
       menu = body.querySelector('section'),
       toggle = body.querySelector(".toggle");
